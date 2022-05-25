51a1d943c8a8c6aa241116a420f5b5ff748b2583
opuopu <nurmdopu428@gmail.com>
2022-05-25 20:08:05 +0600

    filterItem

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,5 +1,5 @@
 import { View,StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
 import Text from '../Text/Text'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../Header'
@@ -9,16 +9,42 @@ import { PLANET_LIST } from '../../../files'
 import { spacing } from '../../theme/Spacing'
 import { AntDesign } from '@expo/vector-icons';
 import Pressable from 'react-native/Libraries/Components/Pressable/Pressable'
+import { TextInput } from 'react-native'
 
 
 export default function Home({navigation}) {
 
+  const [List,setList] = useState(PLANET_LIST)
+
+  // filter name using itemname
+const searchName = (text) =>{
+  const filterName = PLANET_LIST.filter(item =>{
+
+    const textdata = text.toLocaleLowerCase();
+    const itemname = item.name.toLocaleLowerCase();
+    return itemname.indexOf(textdata) >-1
+    // then render filtering item
+
+
+  })
+  setList(filterName)
+}
+
+
+
   return (
 <SafeAreaView style={{flex:1,backgroundColor:colors.black}}>
 <Header ></Header>
+<TextInput
+placeholder="Type the planet name"
+placeholderTextColor={colors.white}
+style={style.input}
+onChangeText={newText => searchName(newText)}
+
+/>
 <FlatList
 contentContainerStyle={style.list}
-data={PLANET_LIST}
+data={List}
 keyExtractor={item => item.name}
 
 renderItem={({item,index})=>{
@@ -75,5 +101,13 @@ texts:{
          flexDirection:'row',
          alignItems:'center',
          justifyContent:'space-between'
+  },
+  input:{
+  padding:spacing[4],
+  borderBottomColor:'white',
+  borderBottomWidth:0.7,
+  color:colors.white,
+  
+ 
   }
 })
